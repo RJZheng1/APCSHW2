c10e725e2e23e3889a4d8455e078e992dc496a58
RJZheng1 <stuykid2016@yahoo.com>
2015-02-07 16:51:56 -0500

    sqrt method no longer uses ==. sqrt method returns 0 if input is 0 to avoid division by zero. fact method will return 1 if input is 0.

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -8,7 +8,7 @@ public class Recursion{
 	return fact2(n,1);
     }
     public int fact2(int n,int ans){
-	if(n == 1)
+	if(n <= 1)
 	    return ans;
 	return fact2(n-1,ans*n);
     }
@@ -25,10 +25,12 @@ public class Recursion{
     public double sqrt(double n){
 	if(n < 0)
 	    throw new IllegalArgumentException();
+	if(n == 0)
+	    return 0;
 	return sqrt2(n,n/2);
     }
     public double sqrt2(double n,double guess){
-	if(guess*guess == n)
+	if(Math.abs(guess*guess-n)/n <= 0.01)
 	    return guess;
 	return sqrt2(n,(n / guess + guess)/ 2);
     }
