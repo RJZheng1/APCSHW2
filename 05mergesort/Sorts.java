4ee739034ae34d9194e95d54640165e0a1c18c69
RJZheng1 <RJZheng1@users.noreply.github.com>
2015-03-04 12:30:47 -0500

    Changed driver for Sorts

--- 05mergesort/Sorts.java
+++ 05mergesort/Sorts.java
@@ -2,22 +2,23 @@ import java.util.*;
 
 public class Sorts{
     public static void main(String[] args){
-	int[][] tests = new int[4][10];
-	tests[0] = new int[]{0,1,2,3,4,5,6,7,8,9};
-	tests[1] = new int[]{9,8,7,6,5,4,3,2,1,0};
+	int[] test;
+	int whichSort;
+	try{
+	    test = new int[Integer.parseInt(args[0])];
+	    whichSort = Integer.parseInt(args[1]);
+	}catch(ArrayIndexOutOfBoundsException e){
+	    test = new int[1000000];
+	    whichSort = 0;
+	}
 	Random r = new Random();
-	for(int x = 0;x < 10;x++){
-	    tests[2][x] = r.nextInt(3) + 1;
+	for(int x = 0;x < test.length;x++){
+	    test[x] = r.nextInt(2000000)-1000000;
 	}
-	for(int x = 0;x < 10;x++){
-	    tests[3][x] = r.nextInt(2000000) - 1000000;
-	}
-	for(int n = 0;n < tests.length;n++){
-	    mergeSort(tests[n]);
-	    for(int x = 0;x < tests[n].length;x++)
-		System.out.print(tests[n][x] + " ");
-	    System.out.println("");
-	}
+	if(whichSort == 0)
+	    Sorts.mergeSort(test);
+	else
+	    Arrays.sort(test);
     }
 
     public static void mergeSort(int[] array){
