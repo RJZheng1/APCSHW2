2d9e0a4b1997414465d303856f5b237690e900c6
RJZheng1 <RJZheng1@users.noreply.github.com>
2015-05-12 12:29:03 -0400

    Did add method.

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class MyHeap{
+    private int[] data;
+    private boolean isMax;
+    
+    public MyHeap(){
+	this(true);
+    }
+
+    public MyHeap(boolean isMax){
+	data = new int[10];
+	this.isMax = isMax;
+    }
+
+    public boolean compare(int i1, int i2){
+	if(isMax)
+	    return data[i1] > data[i2];
+	return data[i1] > data[i2];
+    }
+
+    public void add(int n){
+	data[0] = data[0] + 1;
+	data[data[0]] = n;
+	pushUp(data[0]);
+    }
+
+    public void pushUp(int index){
+	if(index != 1 && compare(index, index/2)){
+	    int swap = data[index];
+	    data[index] = data[index/2];
+	    data[index/2] = swap;
+	    pushUp(index/2);
+	}
+    }
+
+    public String toString(){
+	return Arrays.toString(data);
+    }
+
+    public static void main(String[] args){
+	MyHeap h = new MyHeap();
+	h.add(1);
+	System.out.println(h);
+	h.add(51);
+	System.out.println(h);
+	h.add(25);
+	System.out.println(h);
+	h.add(4);
+	System.out.println(h);
+    }
+}
